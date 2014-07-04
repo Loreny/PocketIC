21c3253ffe43917951aa74beb46e0b5e47f7b9b7
Loreny <kish.kirill@gmail.com>
2014-07-04 20:59:04 +0300

    удалил лишние файлы

--- MaterialsGroup_test/app/src/main/java/com/leteam/materialsgroup_test/MainActivity.java
+++ MaterialsGroup_test/app/src/main/java/com/leteam/materialsgroup_test/MainActivity.java
@@ -2,13 +2,22 @@ package com.leteam.materialsgroup_test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
+
+//    final String LOG_TAG = "myLogs";
+
+    ListView lvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        lvMain = (ListView) findViewById(R.id.lvMain);
+
+
     }
 }
